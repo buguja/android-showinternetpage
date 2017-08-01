cd9a647b780cd5961eab71cf91a9053bd61a05b5
Javier Buron <javier.buron@copyleft.com.mx>
2017-08-01 18:44:04 -0500

    Show google.com with WebView

--- app/src/main/java/com/example/javier/showinternetpage/MainActivity.java
+++ app/src/main/java/com/example/javier/showinternetpage/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.javier.showinternetpage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    WebView wv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        wv = (WebView) findViewById(R.id.wv);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setFocusable(true);
+        wv.setFocusableInTouchMode(true);
+        wv.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        wv.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        wv.getSettings().setDomStorageEnabled(true);
+        wv.getSettings().setDatabaseEnabled(true);
+        wv.getSettings().setAppCacheEnabled(true);
+        wv.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        wv.loadUrl("https://www.google.com");
+        wv.setWebViewClient(new WebViewClient());
     }
 }
